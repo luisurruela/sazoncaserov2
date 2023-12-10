367a9cac1ebeb7a233028af6b95448e15c50361d
Luis Urruela <luis.urruela@gmail.com>
2023-12-09 18:22:36 -0600

    Firebase added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"sazoncasero","appId":"1:1049918271632:web:796d42dc058a5118f82815","storageBucket":"sazoncasero.appspot.com","apiKey":"AIzaSyDFea8CiRGgKfg9SNpvONk_LYq65oRXx-g","authDomain":"sazoncasero.firebaseapp.com","messagingSenderId":"1049918271632"}))), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideStorage(() => getStorage()))]
 };
