250285ca3e8c211a28fa7ddeeaa07c8b3da29c8e
Luis Urruela <luis.urruela@gmail.com>
2023-12-17 11:11:14 -0600

    updated form validation

--- src/app/pages/login/login/login.component.ts
+++ src/app/pages/login/login/login.component.ts
@@ -2,18 +2,19 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { LogoComponent } from '../../../components/logo/logo.component';
 import { AuthService } from '../../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, LogoComponent],
+  imports: [FormsModule, LogoComponent, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   email: string = '';
   password: string = '';
-  formIsValid = false;
+  errorMessage = '';
 
   constructor(
     private auth: AuthService
@@ -21,10 +22,15 @@ export class LoginComponent {
   }
 
   onChange() {
-    this.formIsValid = this.email.trim().length > 0 && this.password.trim().length > 0;
+    this.errorMessage = '';
   }
 
   submit() {
-    this.auth.login(this.email, this.password);
+    this.auth.login(this.email, this.password).then(response => {
+      console.log(response);
+      if (response) {
+        this.errorMessage = response;
+      }
+    });
   }
 }
