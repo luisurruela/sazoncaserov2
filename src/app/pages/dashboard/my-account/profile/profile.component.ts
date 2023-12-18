e7909ed48b50bc7d1cf7e6a8f09617fd4b199ba5
Luis Urruela <luis.urruela@gmail.com>
2023-12-17 19:34:48 -0600

    profile form added

--- src/app/pages/dashboard/my-account/profile/profile.component.ts
+++ src/app/pages/dashboard/my-account/profile/profile.component.ts
@@ -1,16 +1,38 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { bootstrapArrowLeftShort } from '@ng-icons/bootstrap-icons';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { AuthService } from '../../../../services/auth.service';
+import { userProfile } from '../../dashboard/types/user-profile';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, NgIconComponent],
+  imports: [RouterLink, RouterLinkActive, NgIconComponent, ReactiveFormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
   viewProviders: [provideIcons({ bootstrapArrowLeftShort })]
 })
 export class ProfileComponent {
+  profileForm: FormGroup;
 
+  constructor(
+    private auth: AuthService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.profileForm = this.formBuilder.group({
+      displayName: ['', [Validators.required]]
+    });
+  }
+
+  submit() {
+    const userProfile: userProfile = {
+      displayName: this.profileForm.value.displayName
+    } 
+    
+    this.auth.changeUserProfile(userProfile);
+    this.router.navigate(['/dashboard/account']);
+  }
 }
